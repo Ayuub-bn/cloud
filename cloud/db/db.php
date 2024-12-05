4395f3447e8aaa1eb3811ac4086ad1c003b19abe
Ayuub-bn <92815733+Ayuub-bn@users.noreply.github.com>
2024-12-06 00:03:20 +0100

    Update db.php

--- cloud/db/db.php
+++ cloud/db/db.php
@@ -1,6 +1,3 @@
 <?php
-$connectionInfo = array("UID" => "ayoub", "pwd" => "Azerty1-1azerty", "Database" => "cloud", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:cloudtp1.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-
-session_start();
+phpinfo();
+?>
